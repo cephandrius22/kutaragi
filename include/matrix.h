a147a2f0be928ca411506c147f44a1a3c25a2556
Joachim Wright <joewright1610@gmail.com>
2022-12-08 01:33:23 -0500

    Modify the vector class to use std::array internally
    This seems slower than using multiple floats, which
    is probably not an issue but I want to make note of
    it.

--- include/matrix.h
+++ include/matrix.h
@@ -5,13 +5,24 @@
 #ifndef KUTARAGI_MATRIX_H
 #define KUTARAGI_MATRIX_H
 
+#include <vec.h>
 #include <array>
 
 class Mat4 {
 public:
     std::array<float, 4*4> values;
 
+    Mat4() = default;
     Mat4(std::array<float, 16> values) : values(values) {}
+
+    Mat4 operator*(const Vec4& vec) {
+        Mat4 result{};
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                result.values[i * 4 + j] = this->values[i * 4 + j] * vec[j];
+            }
+        }
+    }
 };
 
 #endif //KUTARAGI_MATRIX_H
--- include/vec.h
+++ include/vec.h
@@ -5,56 +5,71 @@
 #ifndef KUTARAGI_VEC_H
 #define KUTARAGI_VEC_H
 
+#include <array>
+
 class Vec3 {
 public:
-    float x, y, z;
+    std::array<float, 3> e;
 
-    Vec3() : x(0), y(0), z(0) {}
-    Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+    Vec3() : e{0, 0, 0} {}
+    Vec3(float x, float y, float z) : e{x, y, z} {}
 
     Vec3 operator+(const Vec3& v) {
-        return Vec3(this->x + v.x, this->y + v.y, this->z + v.z);
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
     }
 
     Vec3 operator-(const Vec3& v) {
-        return Vec3(this->x - v.x, this->y - v.y, this->z - v.z);
+        e[0] -= v.e[0];
+        e[1] -= v.e[1];
+        e[2] -= v.e[2];
+        return *this;
     }
 
+    /* I don't know that this type of multiplication is ever used. */
     Vec3 operator*(const Vec3& v) {
-        return Vec3(this->x * v.x, this->y * v.y, this->z * v.z);
+        e[0] *= v.e[0];
+        e[1] *= v.e[1];
+        e[2] *= v.e[2];
+        return *this;
     }
 
     Vec3 operator*(float scalar) {
-        return Vec3(this->x * scalar, this->y * scalar, this->z * scalar);
+        e[0] *= scalar;
+        e[1] *= scalar;
+        e[2] *= scalar;
+        return *this;
     }
 
     bool operator==(const Vec3& v) {
-        return this->x == v.x && this->y == v.y && this->z == v.z;
+        return this->e == v.e;
     }
 };
 
 using Point3 = Vec3;
 
 float dot(const Vec3& a, const Vec3& b) {
-    return a.x * b.x + a.y * b.y + a.z * b.z;
+    return a.e[0] * b.e[0] + a.e[1] * b.e[1] + a.e[2] * b.e[2];
 }
 
 class Vec4 {
 public:
-    float x, y, z, w;
+    std::array<float, 4> e;
 
-    Vec4() : x(0), y(0), z(0), w(0) {}
-    Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+    Vec4() : e{0, 0, 0, 0} {}
+    Vec4(float x, float y, float z, float w) : e{x, y, z, w} {}
 
     Vec4(Vec3 vec, float w) {
-        this->x = vec.x;
-        this->y = vec.y;
-        this->z = vec.z;
-        this->w = w;
+        this->e[0] = vec.e[0];
+        this->e[1] = vec.e[1];
+        this->e[2] = vec.e[2];
+        this->e[3] = w;
     }
 
     bool operator==(const Vec4& v) {
-        return this->x == v.x && this->y == v.y && this->z == v.z && this->w == v.w;
+        return this->e == v.e;
     }
 };
 
